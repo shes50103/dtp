8cd7b51f19d730c0e71152b2990d098a6164c885
shes50103 <shes50103@gmail.com>
2019-10-09 10:27:55 +0800

    datetime picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require moment
+//= require bootstrap-datetimepicker-3
 //= require_tree .
